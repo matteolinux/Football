4799f7f01d2457dbf36cbe4d5331b10272f32ce3
Matteo <matteo.linux@gmail.com>
2025-01-30 23:13:05 +0100

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by matteo on 30/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("What is Football to you?")
+                .foregroundStyle(.green)
+                //.font(.custom("Arial", size: 30))
+                .font(.largeTitle)
+            HStack{
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    //.frame(width: 100, height: 100)
+                    .foregroundStyle(.blue)
+                Image(systemName: "figure.australian.football")
+                    .resizable()
+                    .scaledToFit()
+                    //.frame(width: 100, height: 100)
+                    .foregroundStyle(.indigo)
+                Image(systemName: "figure.indoor.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    //.frame(width: 100, height: 100)
+                    .foregroundStyle(.purple)
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by matteo on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
